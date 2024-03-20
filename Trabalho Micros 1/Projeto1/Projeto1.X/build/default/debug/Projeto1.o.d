51781b5e19ebc18144dca4807ef899d0eda87cbc
Gilson Almeida <58306248+gilsonfiho@users.noreply.github.com>
2024-03-20 16:34:22 -0300

    Delete Trabalho Micros 1/Projeto1/Projeto1.X/build/default directory

--- Trabalho Micros 1/Projeto1/Projeto1.X/build/default/debug/Projeto1.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/Projeto1.o:  \
-	C:/Users/Gilson/MPLABXProjects/Projeto1.X/Projeto1.asm  \
-
--- Trabalho Micros 1/Projeto1/Projeto1.X/build/default/production/Projeto1.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/Projeto1.o:  \
-	C:/Users/Gilson/MPLABXProjects/Projeto1.X/Projeto1.asm  \
-
